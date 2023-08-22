975c505fcbc2d426353f3ae06d46d282eaba461c
mohi14 <mohiuddinkhan1409@gmail.com>
2023-08-22 19:10:21 +0600

    done

--- src/components/ui/ButtonsContainer.jsx
+++ src/components/ui/ButtonsContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ButtonsContainer = ({generatePDF, setShowSignUpModal }) => {
+const ButtonsContainer = ({ generatePDF, setShowSignUpModal }) => {
   return (
     <div className="border-t border-[#cccccc] mt-[35px] p-[24px] flex justify-between gap-[10px]">
       <button className="rounded-[8px] border border-[#4368AA] w-full text-[16px] text-[#4368AA] font-[600] py-[12px]">
@@ -10,9 +10,12 @@ const ButtonsContainer = ({generatePDF, setShowSignUpModal }) => {
         className="rounded-[8px] border border-[#4368AA] w-full text-[16px] text-[#4368AA] font-[600] py-[12px]"
         onClick={() => setShowSignUpModal(true)}
       >
-        Subscription
+        Full Version
       </button>
-      <button onClick={() => generatePDF()} className="rounded-[8px]  bg-[#4368AA] w-full text-[16px] text-white font-[600] py-[12px]">
+      <button
+        onClick={() => generatePDF()}
+        className="rounded-[8px]  bg-[#4368AA] w-full text-[16px] text-white font-[600] py-[12px]"
+      >
         <a href="/src/assets/files/demo.pdf" download="demo.pdf">
           Download
         </a>
