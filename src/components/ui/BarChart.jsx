539f078351049add14ff330d8f5ce28879442853
MamtazDev <akter.mstmamtaz@gmail.com>
2023-09-13 15:45:54 +0600

    some issues solved

--- src/components/ui/BarChart.jsx
+++ src/components/ui/BarChart.jsx
@@ -1,3 +1,64 @@
+// import React, { useContext } from "react";
+// import {
+//   BarChart,
+//   Bar,
+//   Cell,
+//   XAxis,
+//   YAxis,
+//   CartesianGrid,
+//   Tooltip,
+//   Legend,
+//   ResponsiveContainer,
+// } from "recharts";
+// import { FolderContext } from "../../contexts/FolderContext";
+
+// const BarChartBar = () => {
+//   const { chartData } = useContext(FolderContext);
+//   console.log("chartData", chartData)
+
+  
+
+//   const yAxisTickValues = [0, 20, 40, 60, 80, 100];
+//   const yAxisDomain = [0, 20, 40, 60, 80, 100];
+
+//   const formatYAxisTick = (tick) => `${tick}%`;
+
+//   return (
+//     <div className="h-[300px] w-full">
+//       {chartData.length > 0 && (
+//         <ResponsiveContainer width="100%" height="100%">
+//           <BarChart
+//             width={500}
+//             height={300}
+//             data={chartData}
+//             margin={{
+//               top: 20,
+//               right: 30,
+//               left: 20,
+//               bottom: 5,
+//             }}
+//           >
+//             <CartesianGrid strokeWidth={0.5} />
+//             <XAxis dataKey="name" />
+//             <YAxis
+//               ticks={yAxisTickValues}
+//               domain={yAxisDomain}
+//               tickFormatter={formatYAxisTick}
+//             />
+//             <Legend />
+//             <Bar dataKey="Hard Coral" stackId="a" fill="#63b2fd" />
+//             <Bar dataKey="Soft Coral" stackId="a" fill="#9be0c4" />
+//           </BarChart>
+//         </ResponsiveContainer>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default BarChartBar;
+
+
+
 import React, { useContext } from "react";
 import {
   BarChart,
@@ -9,16 +70,22 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
+  ReferenceLine,
 } from "recharts";
 import { FolderContext } from "../../contexts/FolderContext";
 
 const BarChartBar = () => {
   const { chartData } = useContext(FolderContext);
+  console.log("chartData", chartData);
 
   const yAxisTickValues = [0, 20, 40, 60, 80, 100];
   const yAxisDomain = [0, 20, 40, 60, 80, 100];
 
   const formatYAxisTick = (tick) => `${tick}%`;
+
+  const averageValue =
+    chartData.reduce((total, entry) => total + entry["Hard Coral"] + entry["Soft Coral"], 0) /
+    (chartData.length * 2);
 
   return (
     <div className="h-[300px] w-full">
@@ -45,6 +112,7 @@ const BarChartBar = () => {
             <Legend />
             <Bar dataKey="Hard Coral" stackId="a" fill="#63b2fd" />
             <Bar dataKey="Soft Coral" stackId="a" fill="#9be0c4" />
+            <ReferenceLine y={averageValue} stroke="red" label="Average" />
           </BarChart>
         </ResponsiveContainer>
       )}
@@ -53,3 +121,5 @@ const BarChartBar = () => {
 };
 
 export default BarChartBar;
+
+
